5df0c411dc8c8b7111b70b2a9fa47d90fd7283e7
Spencer Dedrick <spencer.dedrick@gmail.com>
2020-01-25 16:43:36 -0600

    added a link to sourcecode in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,11 @@ import React from "react"
 
 const Footer = () => {
     return(
-        <a target="_blank" rel="noopener noreferrer" href="https://spencerdedrick.netlify.com/" className="footer">Coded By Spencer Dedrick</a>   )
+        <div className="footer">
+        <a target="_blank" rel="noopener noreferrer" href="https://spencerdedrick.netlify.com/" className="footer">Coded By Spencer Dedrick</a>
+        <br/>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/SpencerDedrick/ReactWeatherApp" className="footer">View Source Code</a> 
+        </div>  )
 } 
 
 export default Footer
